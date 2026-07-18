2249a4129fb24e97b10cdcbed10269095a8e069f
rayyab654 <rayyankrens0304@gmail.com>
2026-07-18 20:36:14 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 const App = () => {
   return (
-    <div class="flex flex-col items-center justify-center min-h-screen font-sans p-4">
-      <h1 class="text-4xl font-bold tracking-tight text-white mb-3">
+    <div className="flex flex-col items-center justify-center min-h-screen font-sans p-4">
+      <h1 className="text-4xl font-bold tracking-tight text-white mb-3">
         Rayliziie Media Digital
       </h1>
-      <p class="text-slate-400 text-lg">
+      <p className="text-slate-400 text-lg">
         Sistem sedang dipersiapkan.
       </p>
     </div>
